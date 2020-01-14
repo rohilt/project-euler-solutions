55a5931b0ce34943cf09a9ffd7d6a6d9f9301559
Rohil Tuli <rohil.tuli@gmail.com>
2020-01-13 20:50:35 -0500

    Finished problem 2

--- problem-2.py
+++ problem-2.py
@@ -3,7 +3,13 @@ I will compute the Fibonacci numbers, and add every third number starting with 2
 to sum up only the even numbers. 
 '''
 fib = [1, 2]
-n = 2
+while (fib[len(fib)-1] < 4000000):
+    x = fib[len(fib)-1] + fib[len(fib)-2]
+    fib.append(x)
+fib.pop()
 i = 1
+sum = 0
 while i < len(fib):
+    sum += fib[i]
     i += 3
+print(sum)
